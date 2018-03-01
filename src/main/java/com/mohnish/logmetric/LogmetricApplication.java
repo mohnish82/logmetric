9c2b608bbe49fe4364028b92ee623f0d5d1b6f8a
Mohnish Chaudhary <mohnish82@gmail.com>
2018-02-28 19:21:04 -0500

    initial commit

---
+++ src/main/java/com/mohnish/logmetric/LogmetricApplication.java
@@ -0,0 +1,12 @@
+package com.mohnish.logmetric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogmetricApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogmetricApplication.class, args);
+	}
+}
